dec6e060b0e05ef8fa73efd2a914d66436ea194c
AM GIMINT (Farhan Hamayun) <farhanhamayun@INT.GOV.PK>
2017-08-08 15:55:11 +0500

    testing

--- src/Test.java
+++ src/Test.java
@@ -14,10 +14,125 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class Test {
-
+	
+	static List<String> usernameKeys = Arrays.asList("ctl00$ctl02$tbEmail", "usernmae", "email", "login", "login[username]");
+	
+	static List<String> passwordKeys = Arrays.asList("ctl00$ctl02$tbPassword", "password", "pass", "login[password]");
+	
+	static 	List<String> loginKeys = Arrays.asList("Login", "Signin", "Sign in", "Log in");
+	
+	static 	List<String> logoutKeys = Arrays.asList("Logout", "Signout", "Sign out", "Log out");
+	
+	static String loginUsername = "enquiries@loadmetrics.co.uk";
+	
+	static String loginPassword = "#Unst0n3#";
+	
 	public static void main(String[] args) throws IOException {
 		
-		Connection.Response loginForm = Jsoup.connect("https://www.sportfish.co.uk/customer/account/login/")
+		
+		Connection.Response loginForm = Jsoup.connect("https://www.rightmove.co.uk/login.html")
+                .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1")
+                .method(Connection.Method.GET)
+                .execute();
+
+        Document doc = loginForm.parse();
+
+        Elements hiddenElems = doc.select("input[type=hidden]");
+        Map<String, String> nameValue = new HashMap<>();
+
+        for(Element elem : hiddenElems) {
+            nameValue.put(elem.attr("name"), elem.attr("value"));
+        }
+
+        Document document = Jsoup.connect("https://www.rightmove.co.uk//login.html")
+                .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1")
+                .data("email", loginUsername)
+                .data("password", loginPassword)
+                .data(nameValue)
+                .cookies(loginForm.cookies())
+                .post();
+        
+        System.out.println(document);
+        
+        
+		/*Connection.Response loginForm = Jsoup.connect("http://www.investabroadproperties.com/")
+                .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1")
+                .method(Connection.Method.GET)
+                .execute();
+
+        Document doc = loginForm.parse();
+
+        Elements hiddenElems = doc.select("input[type=hidden]");
+        Map<String, String> nameValue = new HashMap<>();
+
+        for(Element elem : hiddenElems) {
+            nameValue.put(elem.attr("name"), elem.attr("value"));
+        }
+        
+        System.out.println(nameValue);
+
+        Document document = Jsoup.connect("http://www.investabroadproperties.com/")
+                .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1")
+                .data("ctl00$ctl02$tbEmail", "enquiries@loadmetrics.co.uk")
+                .data("ctl00$ctl02$tbPassword", "#Unst0n3#")
+                .data(nameValue)
+//                .data("ctl00$ctl02$btnLogonLogout", "Login")
+                .cookies(loginForm.cookies())
+                .post();
+//                .method(Connection.Method.POST)
+//                .execute();
+        
+        System.out.println(document);*/
+		
+		/*try {
+
+	        Connection.Response response = Jsoup.connect("http://www.investabroadproperties.com/UserLogin.aspx/")
+	                .method(Connection.Method.GET)
+	                .execute();
+
+	        Document doc = response.parse();
+
+	        Elements hiddenElems = doc.select("input[type=hidden]");
+	        Map<String, String> nameValue = new HashMap<>();
+
+	        for(Element elem : hiddenElems) {
+	            nameValue.put(elem.attr("name"), elem.attr("value"));
+	        }
+
+	        Map<String, String> authCookie = response.cookies();
+
+	        response = Jsoup.connect("http://www.investabroadproperties.com/UserLogin.aspx/")
+	                .method(Connection.Method.POST)
+	                .data("ctl00$ctl01$tbEmail", "enquiries@loadmetrics.co.uk") // your email
+	                .data("ctl00$ctl01$tbPassword", "#Unst0n3#") // your password
+//	                .data("__EVENTVALIDATION", eventValidation)
+//	                .data("__VIEWSTATEGENERATOR", viewStateGenerator)
+	                .data("ctl00$ctl01$btnLogonLogout", "Login")
+//	                .data("DES_LinkIDState", desLinkIdState)
+//	                .data("DES_ScriptFileIDState", desScriptFileIDState)
+//	                .data("__VIEWSTATE", viewState)
+	                .data(nameValue)
+	                .cookies(authCookie)
+	                .execute();
+	        
+	        System.out.println("___");
+	        System.out.println(response.body()); // full html response
+
+	        response = Jsoup.connect("http://www.investabroadproperties.com/UserHome.aspx")
+	                .method(Connection.Method.GET)
+	                .cookies(authCookie)
+	                .execute();
+
+	        System.out.println(response.parse().select("div#members_switch").text());
+	        System.out.println("___");
+	        System.out.println(response.body()); // full html response
+
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }*/
+		
+		
+		/*Connection.Response loginForm = Jsoup.connect("https://www.sportfish.co.uk/customer/account/login/")
 				.userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1")
 	            .method(Connection.Method.GET)
 	            .execute();
@@ -37,7 +152,7 @@ public class Test {
 		
 		for(Element elem : hiddenElems) {
 			nameValue.put(elem.attr("name"), elem.attr("value"));
-		}
+		}*/
 		
 	    /*Document document = Jsoup.connect("http://www.investabroadproperties.com/")
 	    		.userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1")
@@ -314,18 +429,65 @@ public class Test {
 	
 	public static Boolean isLoginSuccessful(Document document, AuthenticationForm loginForm) {
 		
-		
-		
 		if(loginForm != null) {
 			return Boolean.FALSE;
 		}
 		
+		// Locate authentication form
+		Object form = null;
+		form = findAndFillForm(document, usernameKeys, passwordKeys, loginKeys, loginUsername, loginPassword);
+		if(form != null) {
+			return Boolean.FALSE;
+		}
+		
+		// Locate logout button/link, if found then return true
+		
+		// Locate error message if found the return false
 		
 		
 		return Boolean.TRUE;
 	}
 	
-	public static AuthenticationForm findForm(Document document, List<String> usernameKeys, List<String> passwordKeys, List<String> loginKeys) {
+	public static Document makeRequest(String loginLink, String doaminUrl) throws Exception {
+		
+		Connection.Response loginForm = Jsoup.connect("https://www.rightmove.co.uk/login.html")
+                .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1")
+                .method(Connection.Method.GET)
+                .execute();
+
+        Document doc = loginForm.parse();
+        
+        AuthenticationForm form = findAndFillForm(doc, usernameKeys, passwordKeys, loginKeys, loginUsername, loginPassword);
+        
+        Document document = Jsoup.connect("https://www.rightmove.co.uk//login.html")
+                .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1")
+                .data(form.getData())
+                .cookies(loginForm.cookies())
+                .post();
+        
+        Boolean isSuccessful = isLoginSuccessful(document, form);
+        
+        if(isSuccessful) {
+        	return document;
+        }
+		
+		return null;
+	}
+	
+	public static String beautifyActionUrl(String actionUrl, String domainUrl) {
+		if(actionUrl.startsWith("http://") || actionUrl.startsWith("https://")) {
+			return actionUrl;
+		}
+		
+		if(actionUrl.equals("/") || actionUrl.equals("./")) {
+			return domainUrl;
+		}
+		
+		
+		return (domainUrl + actionUrl);//.replace("/([^:]\\/)\\/+/g", "$1");
+	}
+	
+	public static AuthenticationForm findAndFillForm(Document document, List<String> usernameKeys, List<String> passwordKeys, List<String> loginKeys, String username, String password) {
 		
 		AuthenticationForm loginForm = null;
 		
@@ -352,17 +514,33 @@ public class Test {
 								for(String loginKey : loginKeys) {
 									
 									if(loginButton.size() > 0) {
-										if(loginKey.equalsIgnoreCase(loginButton.attr("title")) || loginKey.equalsIgnoreCase(loginButton.text())
-											|| loginButton.html().contains(loginKey) || loginKey.equalsIgnoreCase(loginButton.attr("value"))) {
+										if(loginKey.equalsIgnoreCase(loginButton.first().attr("title")) || loginKey.equalsIgnoreCase(loginButton.first().text())
+											|| loginButton.first().html().contains(loginKey) || loginKey.equalsIgnoreCase(loginButton.first().attr("value"))) {
 											// Login button/link exists
+											
+											Elements hiddenElems = myForm.select("input[type=hidden]");
+									        Map<String, String> nameValue = new HashMap<>();
+
+									        for(Element elem : hiddenElems) {
+									            nameValue.put(elem.attr("name"), elem.attr("value"));
+									        }
+									        
+									        loginForm.setData(nameValue);
+									        
+									        nameValue.put(passwordElems.first().attr("name"), loginPassword);
+									        nameValue.put(usernameElems.first().attr("name"), loginUsername);
+									        
+									        if(loginButton.first().attr("name") != null || loginButton.first().attr("name") != "") {
+									        	nameValue.put(loginButton.first().attr("name"), loginButton.first().attr("value"));
+									        }
 											
 											loginForm = new AuthenticationForm();
 											
+											loginForm.setData(nameValue);
 											loginForm.setForm(myForm);
-											loginForm.setPassword(passwordKey);
 											loginForm.setPasswordField(passwordElems.first());
-											loginForm.setUsername(usernameKey);
 											loginForm.setUsernameField(usernameElems.first());
+											loginForm.setLoginBtnField(loginButton.first());
 											
 											break;
 										}
